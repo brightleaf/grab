c4172151efaf8a17099838d1ae0edeb79d9b8523
Kevin Isom <kevin@isom.nz>
2019-04-25 21:47:50 +1200

    Add a delay to make sure any client side code has fired

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer')
+const { delay } = require('@kev_nz/async-tools')
 const slugify = require('@sindresorhus/slugify')
 
 module.exports = async url => {
@@ -11,6 +12,7 @@ module.exports = async url => {
     height: 900,
   })
   await page.goto(url)
+  await delay(1800)
   const title = await page.title()
   const filename = `${slugify(title)}-screenshot.png`
   await page.screenshot({
